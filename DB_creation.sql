77b8a0dbf71dfd85c5b90165a969e9a910d9c1fa
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-01 16:40:30 -0500

    Company Project -01- DB creation->Branch_supplier table created

--- DB_creation.sql
+++ DB_creation.sql
@@ -73,3 +73,17 @@ CREATE TABLE works_with (
 --DROP TABLE works_with;
 
 DESCRIBE works_with;
+
+-- branch_supplier
+
+CREATE TABLE branch_supplier (
+    branch_id INT,
+    supplier_name VARCHAR(40),
+    supply_type VARCHAR(40),
+    PRIMARY KEY(branch_id, supplier_name),
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
+);
+
+--DROP TABLE works_with;
+
+DESCRIBE branch_supplier;
