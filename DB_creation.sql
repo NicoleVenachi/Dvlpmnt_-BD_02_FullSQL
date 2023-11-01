cd90598a54866dd4bd349ad60a956c5737d280f8
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-01 16:37:01 -0500

    Company Project -01- DB creation->Works_with table created

--- DB_creation.sql
+++ DB_creation.sql
@@ -58,3 +58,18 @@ CREATE TABLE client (
 );
 
 DESCRIBE client;
+
+-- client
+
+CREATE TABLE works_with (
+    emp_id INT,
+    client_id INT,
+    total_sales INT,
+    PRIMARY KEY(emp_id, client_id),
+    FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
+
+--DROP TABLE works_with;
+
+DESCRIBE works_with;
