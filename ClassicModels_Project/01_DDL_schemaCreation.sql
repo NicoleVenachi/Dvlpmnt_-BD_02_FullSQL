409ab41235b53a6638b5b0f9822714da05f37ea3
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-09 12:28:20 -0500

    Classic Models Project -02- Data Injection -> offices table data insertion

--- ClassicModels_Project/01_DDL_schemaCreation.sql
+++ ClassicModels_Project/01_DDL_schemaCreation.sql
@@ -1,14 +1,17 @@
 
 USE classicmodels;
+
 -- offices
+DROP TABLE offices;
+
 CREATE TABLE offices (
     officeCode VARCHAR(10) NOT NULL,
     city VARCHAR(50) NOT NULL,
     phone VARCHAR(50) NOT NULL,
     addressLine1 VARCHAR(50) NOT NULL,
     addressLine2 VARCHAR(50),
-    state VARCHAR(50) NOT NULL,
-    country VARCHAR(50) ,
+    state VARCHAR(50),
+    country VARCHAR(50) NOT NULL,
     postalCode VARCHAR(15) NOT NULL,
     territory VARCHAR(10) NOT NULL,
     PRIMARY KEY (officeCode)
