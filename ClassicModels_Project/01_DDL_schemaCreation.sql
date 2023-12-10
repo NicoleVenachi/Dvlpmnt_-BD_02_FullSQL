505e0d65d37eac4aa7560dcb2fa126eb80fda706
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-10 07:57:01 -0500

    Classic Models Project -01- SCHEMAS creation -> customers table created

--- ClassicModels_Project/01_DDL_schemaCreation.sql
+++ ClassicModels_Project/01_DDL_schemaCreation.sql
@@ -33,7 +33,7 @@ CREATE TABLE employees (
     firstName VARCHAR(50) NOT NULL,
     extension VARCHAR(10) NOT NULL,
     email VARCHAR(100) NOT NULL, 
-    officeCode VARCHAR(10) NOT NULL, -- relation with office
+    officeCode VARCHAR(10) NOT NULL, -- relation to an office
     reportsTo INT(11) DEFAULT NULL, -- self relation
     jobTitle VARCHAR(50) NOT NULL,
     PRIMARY KEY (employeeNumber),
@@ -43,3 +43,30 @@ CREATE TABLE employees (
 
 
 DESCRIBE employees;
+
+-- cutomers 
+
+DROP TABLE IF EXISTS  customers; 
+
+CREATE TABLE IF NOT EXISTS customers ( -- doesn't deletes all data like dorp table does
+
+    customerNumber INT(11) NOT NULL,
+    customerName VARCHAR(50) NOT NULL,
+    `contactLastName` VARCHAR(50) NOT NULL,
+    `contactFirstName` VARCHAR(50) NOT NULL,
+    `phone` VARCHAR(50) NOT NULL,
+    `addressLine1` VARCHAR(50) NOT NULL,
+    `addressLine2` VARCHAR(50) NULL DEFAULT NULL,
+    `city` VARCHAR(50) NOT NULL,
+    `state` VARCHAR(50) NULL DEFAULT NULL,
+    `postalCode` VARCHAR(15) NULL DEFAULT NULL,
+    country VARCHAR(50) NOT NULL,
+    salesRepEmployeeNumber INT(11) NULL, -- relation to an employee
+    creditLimit DOUBLE NULL DEFAULT NULL,
+    `customerLocation` POINT NOT NULL,
+    PRIMARY KEY (`customerNumber`),
+    FOREIGN KEY (`salesRepEmployeeNumber`) REFERENCES `employees` (`employeeNumber`)
+);
+
+
+DESCRIBE customers;
