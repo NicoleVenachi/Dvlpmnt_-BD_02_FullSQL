e852882da80e606779f440e7c7cd7730bd8bb2f7
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-14 18:14:32 -0500

    Classic Models Project -03- Querying and its clauses -> Excercises testing WHERE clause

--- ClassicModels_Project/03_DML_Querying.sql
+++ ClassicModels_Project/03_DML_Querying.sql
@@ -22,4 +22,36 @@ FROM employees;
 
 UPDATE employees
 SET reportsTo="1002"
-WHERE jobTitle='Sales Rep' AND officeCode='1';
+WHERE jobTitle='Sales Rep' AND officeCode='1'; -- WHERE on -U-pdate
+
+
+SELECT * 
+FROM employees;
+
+-- **** Excercises ***
+
+SHOW TABLES;
+
+-- list USA customers with credit higher than 1000
+DESCRIBE customers;
+
+SELECT *
+FROM customers
+WHERE country="USA" AND creditLimit >= 1000; 
+
+-- list the employee codes for sales representatives of customers in Spain, France and Italy. Make another query to list the names and email addresses of those employees.
+
+
+SELECT salesRepEmployeeNumber
+FROM customers
+WHERE country IN ("SPAIN", "FRANCE", "ITALY" );
+
+DESCRIBE employees;
+
+SELECT firstName, lastName,  email
+FROM employees
+WHERE employeeNumber IN (
+    SELECT salesRepEmployeeNumber
+    FROM customers
+    WHERE country IN ("SPAIN" OR "FRANCE" OR "ITALY" )
+); --using NESTED QUERY to get the employees ID to look for
