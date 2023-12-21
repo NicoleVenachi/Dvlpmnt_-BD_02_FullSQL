5d377aecc090f36c1ab783357e3baff7a72e2527
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-20 21:23:10 -0500

    Classic Models Project -03- Aggregation and Joins -> Aggregation III, use case COUNT and DISTINCT, couniting how many categories/distict values are in a column

--- ClassicModels_Project/04_DML_AggregtionAndJoins.sql
+++ ClassicModels_Project/04_DML_AggregtionAndJoins.sql
@@ -4,6 +4,41 @@ USE classicmodels;
 
 SHOW TABLES;
 
+-- *** COUNT  *** 
+-- USE CASE counting how many registers fulffil the condition/query
+
+
+-- Report the total number of payments received before October 28, 2004.
+
+SELECT *
+FROM payments;
+
+SELECT COUNT(*)
+FROM payments
+WHERE paymentDate< "2004-10-28";
+
+
+
+
+-- *** COUNT & DISTINCT  *** 
+-- USE CASE counting the number of categories/ distict values in a column
+
+-- Report the number of customer who have made payments before October 28, 2004.
+
+SELECT customerNumber
+FROM payments
+WHERE paymentDate< "2004-10-28"; 
+
+
+SELECT DISTINCT customerNumber
+FROM payments
+WHERE paymentDate< "2004-10-28"; -- unique customer numbers
+
+SELECT COUNT (DISTINCT customerNumber)
+FROM payments
+WHERE paymentDate< "2004-10-28"; -- how many unique customer numbers
+
+
 -- *** AGGREGATION Excercises ***
 
 -- How many employees are there in the company?
@@ -18,11 +53,5 @@ DESCRIBE payments;
 SELECT SUM(amount)
 FROM payments;
 
--- Report the total number of payments received before October 28, 2004.
-SELECT *
-FROM payments;
 
-SELECT COUNT(*)
-FROM payments
-WHERE paymentDate< "2004-10-28"; -- count how many registers fulffil the condition/query
 
