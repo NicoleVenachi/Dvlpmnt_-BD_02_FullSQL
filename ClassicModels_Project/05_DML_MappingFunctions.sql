14c8b5c6cc99edc3286b472bc69e781cba49801d
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-26 16:50:14 -0500

    Classic Models Project -05- Mapping function -> For strings, LCASE and SUBSTRING

--- ClassicModels_Project/05_DML_MappingFunctions.sql
+++ ClassicModels_Project/05_DML_MappingFunctions.sql
@@ -2,7 +2,7 @@ SHOW DATABASES;
 
 USE classicmodels;
 
--- *** Mapping Functions *** 
+-- *** Mapping Functions -> Strings *** 
 
 -- *** UCASE and CONCAT ***
 -- Display the full name of point of contact each customer in the United States in upper case, along with their phone number, sorted by alphabetical order of customer name.
@@ -13,3 +13,14 @@ SELECT customerName, CONCAT (UCASE(contactLastName), ' ', UCASE(contactFirstName
 FROM customers
 WHERE country = 'USA'
 ORDER BY customerName ASC; 
+
+-- *** SUBSTRING and LCASE **
+-- Display a paginated list of customers (sorted by customer name), with a country code column. The country is simply the first 3 letters in the country name, in lower case.
+
+DESCRIBE customers;
+
+SELECT customerName, SUBSTRING(LCASE(country), 1,3) AS countryCode 
+FROM customers
+ORDER BY customerName ASC
+LIMIT 10
+OFFSET 0;
