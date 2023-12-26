d5333fcdebabc42e5f569b51fa51d369d786d3c4
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-26 18:59:28 -0500

    Classic Models Project -06- Joins -> Excercises I, Inner joins

--- ClassicModels_Project/06_DML_Joins.sql
+++ ClassicModels_Project/06_DML_Joins.sql
@@ -18,3 +18,29 @@ ON payments.customerNumber = customers.customerNumber
 ORDER BY paymentDate DESC
 LIMIT 10
 ;
+
+
+
+-- *** Excercises ***
+-- Show the full office address and phone number for each employee.
+SHOW TABLES;
+
+DESCRIBE employees;
+DESCRIBE offices;
+
+SELECT lastName, jobTitle, offices.officeCode, CONCAT(country, ' ', city) AS dependency, addressLine1, phone AS officePhone
+FROM employees
+JOIN offices
+ON employees.officeCode = offices.officeCode
+; -- registers/existing in both (i.e., a cada empleado le traigo su oficina)
+
+-- Show the full order information and product details for order no. 10100.
+DESCRIBE products;
+DESCRIBE orderdetails;
+
+SELECT orderdetails.*, productName, productVendor
+FROM orderdetails
+JOIN products
+ON orderdetails.productCode = products.productCode
+WHERE orderNumber = 10100
+; -- de la orden, veo todos los productos pedidos, y muestro la info del producto
